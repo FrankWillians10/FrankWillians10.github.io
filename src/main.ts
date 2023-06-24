8f429c93f0b10a9917b57701e1b4d357097f9007
Frank Willians <frankkwillians@gmail.com>
2023-06-23 22:41:23 -0400

    gg

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,4 @@ efeitoMaquinaDeEscrever();
 
     ///assets/index-7be66c24.css
 
-    // /src/main.ts
+    // 
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+import {defineConfg} from "vite";
+
+export default defineConfig({
+  base: "/frankwillians10.github.io/frankwillians10.github.io"
+});
